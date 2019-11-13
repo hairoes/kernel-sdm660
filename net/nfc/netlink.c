8470dd45187d4a41f9f327f49e091e6debf31b0c
meg4z0rd <meydiwahendra@gmail.com>
2019-11-14 01:28:41 +0700

    Merge remote-tracking branch 'upstream/kernel.lnx.4.4.r37-rel' into kernel.lnx.4.4.r37-rel-caf

--- net/nfc/netlink.c
+++ net/nfc/netlink.c
@@ -1064,7 +1064,6 @@ static int nfc_genl_llc_set_params(struct sk_buff *skb, struct genl_info *info)
 
 	local = nfc_llcp_find_local(dev);
 	if (!local) {
-		nfc_put_device(dev);
 		rc = -ENODEV;
 		goto exit;
 	}
@@ -1124,7 +1123,6 @@ static int nfc_genl_llc_sdreq(struct sk_buff *skb, struct genl_info *info)
 
 	local = nfc_llcp_find_local(dev);
 	if (!local) {
-		nfc_put_device(dev);
 		rc = -ENODEV;
 		goto exit;
 	}
